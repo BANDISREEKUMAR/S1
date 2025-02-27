3286d62a5de88fe1ab222b3b489abcd03cb75de3
BANDISREEKUMAR <codewithsree5@gmail.com>
2025-02-27 22:06:12 +0530

    Src Folder updated

--- source/To-Do-Application/Todo/src/App.jsx
+++ source/To-Do-Application/Todo/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
 import './App.css'
-import First from './Components/First.jsx'
-import styles from './Components/first.module.css'
+import Header from './Components/Header.jsx'
 function App() {
   return (
    <div>
-    <h1>Welcome to the world of Tech</h1>
-    <div className={styles.a}>
-    <First />
+    <div className='head'>
+      <Header/>
     </div>
-    
    </div>
   
   )
