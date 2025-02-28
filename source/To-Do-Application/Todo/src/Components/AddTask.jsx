4371e9cf900f45ba35122a4e29a00af1c8d8e38d
surya <suryabirru1@gmail.com>
2025-02-28 22:01:58 +0530

    addtask modified

--- source/To-Do-Application/Todo/src/Components/AddTask.jsx
+++ source/To-Do-Application/Todo/src/Components/AddTask.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import styles from './main.module.css'
 export default function AddTask() {
+  function handleSubmit(e){
+    e.preventDefault();
+  }
   return (
-    <div>
-      <div className={styles.addcomponent}>
-
+    <div className={styles.addcomponent}>
+      <div className={styles.Form}>
+        <form onSubmit={handleSubmit}>
+          <input type="text" required />&nbsp;
+          <button type="submit">Add Task</button>
+          <input type="date"  />
+        </form>
       </div>
     </div>
   )
